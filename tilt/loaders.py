6ec9ad860c1428b8d8145e9152df8581039f3357
Daniel N. Baker <dnb@cs.jhu.edu>
2021-06-08 08:28:17 -0400

    Biased samplers for PyTorch

--- tilt/loaders.py
+++ tilt/loaders.py
@@ -8,6 +8,22 @@ except:
 
 
 def make_even_sampler(labels, idx=None, nsamples=None, replace=True):
+    """
+    make_even_sampler
+        Args:
+            labels: Sequence of labels
+        Keyword Args:
+            idx = None
+                To only sample from specific indices, provide a list or numpy array of indices
+            nsamples = None
+                To change the number of samples, change this to an integer
+            replace = True
+                To avoid sampling with replacement, change this to False
+                Guarantees for gradient sampling estimation are usually based on sampling *with* replacement.
+
+        Returns:
+            torch.utils.data.sampler.WeightedRandomSampler
+    """
     cts = Counter(labels[i] for i in (idx if idx else range(len(labels))))
     if not nsamples:
         nsamples = len(labels)
@@ -15,12 +31,34 @@ def make_even_sampler(labels, idx=None, nsamples=None, replace=True):
 
 
 def make_biased_sampler(weights, nsamples=None, replace=True):
+    """
+    make_biased_sampler
+        Args:
+            labels: Sequence of labels
+        Keyword Args:
+            nsamples = None
+                To change the number of samples, change this to an integer
+            replace = True
+                To avoid sampling with replacement, change this to False
+                Guarantees for gradient sampling estimation are usually based on sampling *with* replacement.
+
+        Returns:
+            torch.utils.data.sampler.WeightedRandomSampler
+    """
     if not nsamples:
         nsamples = len(weights)
     return WeightedRandomSampler(weights, nsamples, replacement=replace)
 
 
 def make_weights(grouping_lists):
+    """
+    make_weights
+    Converts a list of grouping assignments into a set of sampling weights for a collection of data points
+
+        Input: Iterable[Iterable[Int]]
+
+        Output: numpy.ndarray, numpy.float64
+    """
     counts = list(map(Counter, grouping_lists))
     t = []
     for c, l in zip(counts, grouping_lists):
@@ -31,3 +69,5 @@ def make_weights(grouping_lists):
 ## TODO:
 ##  Auto-contrastive pair/triple generation
 ##  Use coresets for selecting SGD methods
+
+__all__ = ["make_even_sampler", "make_biased_sampler", "make_weights"]
